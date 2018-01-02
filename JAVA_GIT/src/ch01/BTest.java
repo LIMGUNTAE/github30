c828572b7945789820899fbe4cb373eeeb910741
KO_03 <KO_03@KO_03-PC>
2018-01-02 12:45:03 +0900

    BTest 세번째 커밋 출력

--- JAVA_GIT/src/ch01/BTest.java
+++ JAVA_GIT/src/ch01/BTest.java
@@ -4,6 +4,7 @@ public class BTest {
 	public static void main(String[] arg) {
 		System.out.println("BTest 첫번째 commit^.^");
 		System.out.println("BTest 두번째 commit(A사용자)");
+		System.out.println("BTest 세번째 commit(B사용자)");
 	}
 
 }
